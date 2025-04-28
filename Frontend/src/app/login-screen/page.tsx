5656831e8d2f1df167f8bc11a774b3fbbfc058ef
Thiago <thiagorafael2005@gmail.com>
2025-04-28 19:09:56 -0300

    erro na imagem do fundo arrumado

--- Frontend/src/app/login-screen/page.tsx
+++ Frontend/src/app/login-screen/page.tsx
@@ -1,12 +1,21 @@
-// import Image from "next/image";
 import React from "react";
-// import ImageBackgroundLogin from "@/assets/img/ImageBackgroundLogin.jpg";
 import { Login } from "@/components/login";
+import BackgroundImage from "@/assets/img/ImageBackgroundLogin.jpg";
+import Image from "next/image";
 
 export default function LoginScreen() {
   return (
-    <div className="h-screen bg-[url(@/assets/img/ImageBackgroundLogin.jpg)] bg-[length:150%] bg-no-repeat bg-center flex justify-center items-center">
-      <Login />
+    <div className="relative h-screen flex justify-center items-center overflow-hidden">
+      <Image
+        src={BackgroundImage}
+        alt=""
+        fill
+        className="object-cover"
+        style={{ zIndex: 0 }}
+      />
+      <div className="relative z-10">
+        <Login />
+      </div>
     </div>
   );
 }
