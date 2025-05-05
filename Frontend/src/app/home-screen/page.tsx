76df53ac320d1ae80605e05dcb2aa930cd550a80
Thiago <thiagorafael2005@gmail.com>
2025-05-05 17:42:14 -0300

    arrumando erro no front

--- Frontend/src/app/home-screen/page.tsx
+++ Frontend/src/app/home-screen/page.tsx
@@ -1,12 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { gsap } from "gsap";
 
 export default function HomeScreen() {
   const welcomeRef = useRef(null);
 
-  useEffect(() => {
-    const welcome = welcomeRef.current;
-  }, []);
   return (
     <div>
       <p>askdsadasd</p>
